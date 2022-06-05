97eed758645a392857470d2d0dd3e044cc703946
Edwin Hanikara <102173815+hanikara@users.noreply.github.com>
2022-06-05 22:26:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,27 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, number) => {
+  const order = number(names);
+  const nameOrder = [];
+  for (var i = 0; i < order.length; i++) {
+    nameOrder[i] = `${i + 1}. + ${order[i]}`;
+  }
+  return nameOrder;
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+  return names.sort();
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
-
+const sortDescending = (names) => {
+  return names.sort().reverse();
+}
+  
 // ! JANGAN DIMODIFIKASI
 (function main() {
   console.log(sorter?.(names, sortAscending)?.join("\n"));
